6ec21d0248249505594522d82b68a23945c7bd7f
ygy <40139861+vviat@users.noreply.github.com>
2018-09-05 23:02:59 +0800

    去掉链表的重复元素

---
+++ 去掉链表的重复元素.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+    int val;
+    struct Node *next;
+} ListNode;
+/**
+ * 已经定义的 ListNode 结点
+ * typedef struct Node {
+ *     int val;
+ *     struct Node *next;
+ * } ListNode;
+ */
+ListNode* linkedListUnique(ListNode *head) {
+    ListNode *p, *q;
+    p = head;
+    while (p) {
+        if (p->next == NULL) break;
+        if (p->val == p->next->val) {
+            q = p->next->next;
+            p->next = q;
+        } else {
+            p = p->next;
+        }
+    }
+    return head;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    ListNode* head = (ListNode*)malloc(sizeof(ListNode));
+    head->val = 0;
+    head->next = NULL;
+    ListNode *temp = head;
+    for (int i = 0; i < n; ++i) {
+        int x;
+        scanf("%d", &x);
+        temp->next = (ListNode*)malloc(sizeof(ListNode));
+        temp->next->val = x;
+        temp->next->next = NULL;
+        temp = temp->next;
+    }
+    temp = head;
+    head = head->next;
+    free(temp);
+    head = linkedListUnique(head);
+    temp = head;
+    while (head) {
+        printf("%d ", head->val);
+        head = head->next;
+        free(temp);
+        temp = head;
+    }
+    printf("\n");
+    return 0;
+}
