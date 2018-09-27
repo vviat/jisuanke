396f710fde08e4fa083dffb7e04a5ab707ec633d
ygy <40139861+vviat@users.noreply.github.com>
2018-09-27 21:00:04 +0800

    矩形面积与周长c++

---
+++ 矩形面积与周长c++.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int main() {
+    int a;
+    int b;
+    cin>>a>>b;
+    cout<<2*(a+b)<<endl<<a*b<<endl;
+
+    return 0;
+}
