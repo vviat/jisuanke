99012778df3043a5d99b6f3caa73f7508ff11fd2
ygy <40139861+vviat@users.noreply.github.com>
2018-09-28 19:35:15 +0800

    圆形面积与周长c++

---
+++ 圆形面积与周长c++.c
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setprecision;
+using std::fixed;
+#define PI 3.14159
+int main() {
+int r;
+    cin>>r;
+    cout<<fixed;
+    //cout<<setprecision(3);
+    cout<<2*PI*r<<endl;
+        cout<<PI*r*r<<endl;
+    
+
+    return 0;
+}
